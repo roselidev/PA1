e5c096b58cc3f9ef99c7a5b0ded2a7627eb9d07a
Jin-Soo Kim <jinsoox@gmail.com>
2019-03-20 15:28:38 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project #1: Converting Fixed-Point to Floating-Point
+## Due: March 31, 11:59PM
---
+++ pa1.c
@@ -0,0 +1,34 @@
+//--------------------------------------------------------------
+// 
+//  4190.308 Computer Architecture (Spring 2019)
+//
+//  Project #1: Converting Fixed-Point to Floating-Point
+//
+//  March 20, 2019
+//
+//  Jin-Soo Kim (jinsoo.kim@snu.ac.kr)
+//  Systems Software & Architecture Laboratory
+//  Dept. of Computer Science and Engineering
+//  Seoul National University
+//
+//--------------------------------------------------------------
+
+
+#include <stdio.h>
+#include "pa1.h"
+
+
+// fix2double() returns the double-precision floating-point value for 
+// the given fixed-point value 'a'. Note that 'a' is represented in 
+// (21.10) fixed-point format.
+
+double fix2double (fixp a)
+{
+
+
+
+
+
+
+	return 3.14;
+}
